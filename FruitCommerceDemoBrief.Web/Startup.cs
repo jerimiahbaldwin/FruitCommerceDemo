311aa00879d239f8345a919c640864471292359f
Jerimiah Baldwin <jjbaldwin@gmail.com>
2020-09-29 18:03:43 -0600

    Add session handling.

--- FruitCommerceDemoBrief.Web/Startup.cs
+++ FruitCommerceDemoBrief.Web/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +24,12 @@ namespace FruitCommerceDemoBrief.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            // Session services.
+            services.AddMemoryCache();
+            services.AddSession();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +45,14 @@ namespace FruitCommerceDemoBrief.Web
             }
 
             app.UseStaticFiles();
+
+            // Session services.
+            app.UseSession();
+            app.Use(async (context, next) =>
+            {
+                context.Session.Set("SessionCreatedDate", BitConverter.GetBytes(new DateTime().Ticks));
+                await next();
+            });
 
             app.UseRouting();
 
